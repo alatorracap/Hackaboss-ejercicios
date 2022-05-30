b3f986da5b4c586cca3f88b2b2ad563ca0c0f94a
Alejandra Latorraca Poblacion <Alejandra@Crit.local>
2022-05-30 11:30:12 +0200

    cucu listo

--- semana-04-javascript/3-Bucles/script.js
+++ semana-04-javascript/3-Bucles/script.js
@@ -1,19 +1,18 @@
 'use strict';
 let clock;
 let time = 1;
+let cucu = 'cucú ';
 
-for (time; time <= 24; time++) {
-  let scream = ' cucú';
+for (time; time <= 3; time++) {
+  /*   
+  repeat is a function that concats the given text
+  doesnt really need a var it can just be 
+  "cucu ".repeat(time);
+  and it would store the repeated concat in the variable scream
+ */
+  let scream = cucu.repeat(time);
 
-  let i = 0;
-  /*
-    mala iteracion lol
-    // while (i < time) {
-    //   scream += scream;
-    //   i++;
-    // }
-    */
-  // scream *= scream;
   console.log(`${scream}`);
+
   console.log(`It's ${time} o'clock!`);
 }
