d924b3ddf825ee1f1904910dc2aa1da5b1fa9f52
Alejandra Latorraca Poblacion <Alejandra@Crit.local>
2022-05-31 16:05:47 +0200

    add extras

---
+++ ejercicios_extra/Vol_1/ejercicio-1.js
@@ -0,0 +1,16 @@
+/* #################
+ * ## Ejercicio 1 ##
+ * #################
+ *
+ * Crea una variable que almacene tu nombre y otra variable que almacene
+ * tu número favorito.
+ *
+ *  - Muestra ambos valores por consola.
+ *
+ *  - Cambia tu nº favorito por cualquier otro nº.
+ *
+ *  - Muestra por consola el resultado del cambio.
+ *
+ *  - Muestra por consola el tipo de las variables definidas.
+ *
+ */
